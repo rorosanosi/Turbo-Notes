9f796ad49119086ea2b1b6bae97e15bee7f07f39
roro <149879370+rorosanosi@users.noreply.github.com>
2026-03-08 08:54:42 +0000

    Refactor UI handling for flashcards and deck creation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// UI 
+// UI - createnew.html 
 const createDeckBtn = document.getElementById('createNewDeck');
 const generatorSection = document.getElementById('generatorSection');
 const activeDeckTitle = document.getElementById('activeDeckTitle');
@@ -11,7 +11,43 @@ createDeckBtn.addEventListener('click',()=>{
     }
 });
 
+// UI - flashcards.html
+const car = document.getElementById('car-container');
+const cardText = car.querySelector('.card-text');
 
+let deck = JSON.parse(localStorage.getItem('activeDeck')) || [];
+let currentIndex = 0;
+let isFlipped = false;
+
+if (deck.length > 0) {
+    showCard();
+} else {
+    cardText.innerText = "No deck found. Go back and create one!";
+}
+
+function showCard() {
+    isFlipped = false;
+    cardText.innerText = deck[currentIndex].front;
+    car.classList.remove('exit');
+    car.classList.add('enter');
+}
+
+document.addEventListener('keydown', (event)=> {
+    if (event.key === "ArrowRight" && deck.length > 0) {
+        car.classList.remove('enter');
+        car.classList.add('exit');
+
+        setTimeout(() => {
+            currentIndex = (currentIndex +1) % deck.length;
+            showCard();
+        }, 600);
+    }
+
+    if (event.key === " "){
+        isFlipped = !isFlipped;
+        cardText.innerText = isFlipped ? deck[currentIndex].back : deck[currentIndex].front;
+    }
+});
 
 // API for flashcard generation
 const generateBtn = document.getElementById('generateBtn');
